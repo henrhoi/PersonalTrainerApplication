8e7dc9c946ff5dd73ad66f385d052d95e2ebd0ca
kristogj <kristoffergjerde@gmail.com>
2018-01-31 14:53:15 +0100

    #2: TestJunit og TestRunner bygges inn i maven og fungerer ved kjøring

---
+++ tdt4140-gr1801/app.core/src/test/java/tdt4140/gr1801/app/core/TestJunit.java
@@ -0,0 +1,16 @@
+package tdt4140.gr1801.app.core;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class TestJunit {
+
+	@Test
+	
+	public void testAdd() {
+		String str = "Junit is working fine";
+		assertEquals("Junit is working fine", str);
+	}
+	
+	
+}
