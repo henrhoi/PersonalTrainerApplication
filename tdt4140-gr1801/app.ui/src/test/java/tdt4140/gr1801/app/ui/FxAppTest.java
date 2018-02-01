eb11143b79dd0b65cf85eb1659c8ebf53ad35590
kristogj <kristoffergjerde@gmail.com>
2018-02-01 15:30:54 +0100

    #4: Updated FxAppTest from example files

--- tdt4140-gr1801/app.ui/src/test/java/tdt4140/gr1801/app/ui/FxAppTest.java
+++ tdt4140-gr1801/app.ui/src/test/java/tdt4140/gr1801/app/ui/FxAppTest.java
@@ -1,5 +1,6 @@
 package tdt4140.gr1801.app.ui;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 
@@ -9,6 +10,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxAppTest extends ApplicationTest {
+	
+	@BeforeClass
+	public static void headless() {
+		if (Boolean.valueOf(System.getProperty("gitlab-ci", "false"))) {
+			System.setProperty("prism.verbose", "true"); // optional
+			System.setProperty("java.awt.headless", "true");
+			System.setProperty("testfx.robot", "glass");
+			System.setProperty("testfx.headless", "true");
+			System.setProperty("glass.platform", "Monocle");
+			System.setProperty("monocle.platform", "Headless");
+			System.setProperty("prism.order", "sw");
+			System.setProperty("prism.text", "t2k");
+			System.setProperty("testfx.setup.timeout", "2500");
+		}
+	}
 
 	@Override
     public void start(Stage stage) throws Exception {
