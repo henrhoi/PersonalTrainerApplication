57b7aba4b2685a81c21e85483364bed2f40bda9d
kristogj <kristoffergjerde@gmail.com>
2018-02-23 19:35:41 +0100

    #8 small updates

--- tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/NewUserController.java
+++ tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/NewUserController.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import tdt4140.gr1801.app.core.PersonalTrainer;
 
@@ -27,6 +30,9 @@ public class NewUserController {
 	@FXML 
 	PasswordField passwordField, confirmPasswordField;
 	
+	@FXML
+	Label infoLabel;
+	
 	
 	//Action on createUserButton
 	@FXML
@@ -39,43 +45,47 @@ public class NewUserController {
 		//Check all fields for errors
 		ArrayList<Boolean> results = PersonalTrainer.checkAllFields(fields);
 		
-		//TODO Show errors in view
+		
+		String info = "";
 		for(int i = 0; i < fields.size(); i++) {
 			if(!results.get(i)) {
-				//Temporary just a print
-				System.err.println("Error in " + fields.get(i).getId());
+				info += info.length() == 0 ? "Error in " + fields.get(i).getId() : ", " +fields.get(i).getId();
 			}
 		}
+		
 		
 		//Check password-confirmation
 		if(!passwordField.getText().equals(confirmPasswordField.getText())){
 			results.add(false);
-			System.err.println("Password not the same");
+			info += info.length() == 0 ? "Error in password" : ", passwordField";
 		}
 		
+		//Show errors in view
+		infoLabel.setText(info);
+		infoLabel.setTextFill(Color.RED);
 		
 		//If all fields is valid, go back to login
 		Set<Boolean> set = new HashSet<Boolean>(results);
 		set.addAll(results);
 		if (set.size() == 1 && set.contains(true)) {
 			//TODO - Create new user object and save user.
-			backToLogin(results);
+			openNewFXML("FxLogin.fxml");
 		}
 		
 	}
 	
-	public void backToLogin(ArrayList<Boolean> results) {
+	public void openNewFXML(String fxmlName) {
 		Parent root;
 		Stage stage = (Stage)this.usernameField.getScene().getWindow();
 		try {
-			root = FXMLLoader.load(getClass().getResource("FxLogin.fxml"));
+			root = FXMLLoader.load(getClass().getResource(fxmlName));
 			Scene scene = new Scene(root);
 	        stage.setScene(scene);
 	        stage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+        
 	}
 	
 	
