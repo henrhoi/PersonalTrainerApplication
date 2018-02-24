0764429bc2ba41694c76bd1e8d2e90886e924cb3
Henrik Høiness <henrhoi@stud.ntnu.no>
2018-02-24 12:58:24 +0100

    #19 - Opprettet Training-klasse, uten noen videre kode.Kun for å kunne referere til klassen.

---
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Training.java
@@ -0,0 +1,13 @@
+package tdt4140.gr1801.app.core;
+
+
+// Er det mulig at denne klassen bør være abstract?
+
+// TODO - Legge til det som er felles for subklassene
+
+
+public class Training {
+	
+	// Superklasse for Endurance og Strength. Vil inneholde alt som er felles for disse
+
+}
