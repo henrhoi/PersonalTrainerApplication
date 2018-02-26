ffd5543b56705c8449f432269fd1a7d3bdc6b114
axeloh <axel.harstad@gmail.com>
2018-02-26 22:54:46 +0100

    #19 Attributter og metoder lagt inn for Training-superklassen.

--- tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Training.java
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Training.java
@@ -1,13 +1,32 @@
 package tdt4140.gr1801.app.core;
 
-
-// Er det mulig at denne klassen bÃ¸r vÃ¦re abstract?
-
-// TODO - Legge til det som er felles for subklassene
+import java.util.Date;
 
 
-public class Training {
+//Superklasse for Endurance og Strength. Vil inneholde alt som er felles for disse
+// Abstrakt da det ikke skal være mulig å instansiere rene Training-objekter
+
+public abstract class Training {
 	
-	// Superklasse for Endurance og Strength. Vil inneholde alt som er felles for disse
+	protected Date date;
+	protected int duration; // I minutter
+	
+	public Training(Date date, int duration) {
+		if (duration < 0) {
+			throw new IllegalArgumentException("Duration cannot be negative");
+		}
+		this.duration = duration;
+		this.date = date;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public int getDuration() {
+		return duration;
+	}
+	
+	
 
 }
