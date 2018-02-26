6e2cd18188605c7fda27dafb75c80daf6aff5aa2
axeloh <axel.harstad@gmail.com>
2018-02-26 23:05:22 +0100

    #18 Attributter og metoder lagt inn for Endurance-subklassen.

--- tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Endurance.java
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Endurance.java
@@ -1,7 +1,43 @@
 package tdt4140.gr1801.app.core;
 
+import java.util.Date;
+
 public class Endurance extends Training{
 	
-	// Legge til det som er s√¶regent for Utholdenhets√∏kter
+	private double distance; // I km
+	private double averageSpeed; // I km/t
+	private int caloriesBurned;
+	
+	
+	public Endurance(Date date, int duration, double distance, int caloriesBurned) {
+		super(date, duration);
+		if (distance < 0) {
+			throw new IllegalArgumentException("Distance cannot be negative");
+		}
+		if (caloriesBurned < 0) {
+			throw new IllegalArgumentException("Calories burned cannot be negative");
+		}
+		this.distance = distance;
+		this.averageSpeed = distance/(duration/60); // duration er i minutter, mÂ deles pÂ 60 for Â fÂ i timer
+		this.caloriesBurned = caloriesBurned;
+		
+		// TODO: mÂlinger av hjertefrekvens
+	}
+	
+	public double getDistance() {
+		return distance;
+	}
+	
+	public double getAverageSpeed() {
+		return averageSpeed;
+	}
+	
+	public int getCaloriesBurned() {
+		return caloriesBurned;
+	}
+	
+	
+	
+	
 
 }
