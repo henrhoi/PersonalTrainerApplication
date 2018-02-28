d05b1d35f432a1c87a0e6748bb8408b16dc6b2c6
Sebastian Øverås <sebastian.overas@gmail.com>
2018-02-28 11:51:01 +0100

    Test with two people on same branch

--- tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Strength.java
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Strength.java
@@ -1,7 +1,13 @@
 package tdt4140.gr1801.app.core;
 
+import java.util.Date;
 
-public class Strength{
+public class Strength extends Training {
+
+	public Strength(Date date, int duration) {
+		super(date, duration);
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 			
