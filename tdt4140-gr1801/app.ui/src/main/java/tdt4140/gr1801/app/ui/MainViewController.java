9f8660a54d6eb5bc7d79b56cff0dcd0e1db11b3d
kristogj <kristoffergjerde@gmail.com>
2018-03-05 11:34:45 +0100

    #25 logoff action added

--- tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/MainViewController.java
+++ tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/MainViewController.java
@@ -3,14 +3,16 @@ package tdt4140.gr1801.app.ui;
 
 
 import java.io.IOException;
-import java.net.URL;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class MainViewController {
 	
@@ -19,6 +21,9 @@ public class MainViewController {
 	
 	@FXML
 	Tab overviewTab, strengthTab, enduranceTab, healthTab, programTab;
+	
+	@FXML
+	Button logOffButton;
 	
 	
 	public void initialize() {
@@ -43,6 +48,27 @@ public class MainViewController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@FXML
+	private void logOff() {
+		//TODO save changes
+		openNewFXML("FxLogin.fxml");
+	}
+	
+	public void openNewFXML(String fxmlName) {
+		Parent root;
+		Stage stage = (Stage)this.logOffButton.getScene().getWindow();
+		try {
+			root = FXMLLoader.load(getClass().getResource(fxmlName));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.show();
+		} catch (IOException e) {
+			System.out.println("hei");
+			e.printStackTrace();
+		}
+        
 	}
 	
 	
