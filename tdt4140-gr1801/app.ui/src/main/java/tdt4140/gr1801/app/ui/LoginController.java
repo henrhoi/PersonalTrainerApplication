634f34aa62c9c65045da1511147ba5d9a471996d
kristogj <kristoffergjerde@gmail.com>
2018-03-07 23:18:01 +0100

    #32 dette er egentlig mye av det som ble gjort i StackPane branchen.
    Etter en mergeconflict ble det mye problemer for branchene som skulle
    merges til master. Velger derfor å pushe direkte til master istedet.

--- tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/LoginController.java
+++ tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/LoginController.java
@@ -1,17 +1,22 @@
 package tdt4140.gr1801.app.ui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 
+import org.apache.http.client.ClientProtocolException;
+
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import tdt4140.gr1801.web.server.LoginModule;
 
-public class LoginController {
+public class LoginController implements Controller{
 	
 	@FXML
 	TextField usernameField;
@@ -19,40 +24,56 @@ public class LoginController {
 	@FXML
 	PasswordField passwordField;
 	
-
+	@FXML
+	Label loginInfo;
 	
-	public void initialize() {
+	@FXML
+	Pane pane;
+	
+	@FXML
+	StackPane stack;
+	
+	//Action on enter from passwordField and loginButton
+	@FXML
+	public void loginAction() throws NoSuchAlgorithmException, ClientProtocolException, IOException {
+		//TODO - Check if username and password are OK
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+		if(LoginModule.checkLogin(username, password)) {
+			//Get current Stage
+			Stage stage = (Stage)this.usernameField.getScene().getWindow();
+			//Get FXML-file
+			URL path = getClass().getResource("FxMainView.fxml");
+			//Make the new Controller
+			MainViewController controller = new MainViewController(username);
+			SceneLoader.setScene(stage, path, controller);
+			
+			//Update all the information for current loggedIn user.
+			controller.updateInfo();
+		}
+		else {
+			loginInfo.setVisible(true);
+			ObservableList<String> userstyle = usernameField.getStyleClass();
+			ObservableList<String> passwordstyle = passwordField.getStyleClass();
+			if(!userstyle.contains("error")) {
+				userstyle.add("error");
+				passwordstyle.add("error");
+			}
+			
+		}
+		
+	}
+	
+	//Action when pressing newUserButton
+	@FXML
+	public void newUserAction() {
+		NewUserController controller = new NewUserController();
+		Stage stage = (Stage)this.usernameField.getScene().getWindow();
+		URL path = getClass().getResource("FxNewUser.fxml");
+		SceneLoader.setScene(stage, path, controller);
 		
 	}
 	
 	
-	//Action on enter from passwordField
-	@FXML
-	public void loginAction() {
-		//TODO - Check if username and password are OK
-		openNewFXML("FxMainView.fxml");
-	}
-	
-	
-	//Action on newUserButton
-	@FXML
-	public void newUserAction() {
-		openNewFXML("FxNewUser.fxml");
-	}
-	
-	public void openNewFXML(String fxmlName) {
-		Parent root;
-		Stage stage = (Stage)this.usernameField.getScene().getWindow();
-		try {
-			root = FXMLLoader.load(getClass().getResource(fxmlName));
-			Scene scene = new Scene(root);
-	        stage.setScene(scene);
-	        stage.show();
-		} catch (IOException e) {
-			System.out.println("hei");
-			e.printStackTrace();
-		}
-        
-	}
 
 }
--- tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/MainViewController.java
+++ tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/MainViewController.java
@@ -1,11 +1,83 @@
 package tdt4140.gr1801.app.ui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
-public class MainViewController {
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class MainViewController implements Controller{
 	
 	@FXML
-	Label mainview;
+	ImageView picture;
+	
+	@FXML
+	Tab overviewTab, strengthTab, enduranceTab, healthTab, programTab;
+	
+	@FXML
+	Button logOffButton, addClientButton;
+	
+	@FXML
+	Label label;
+	
+	private String username = "";
+	
+
+	
+	public void initialize() {
+	}
+	
+	public MainViewController(String username) {
+		this.username = username;
+	}
+	
+	//Used in initialize for setting which fxml-file to open in which tab
+	private void setTab(String fxml, Tab tab) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource(fxml));
+			tab.setContent(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private void logOff() {
+		Stage stage = (Stage) logOffButton.getScene().getWindow();
+		LoginController controller = new LoginController();
+		URL path = getClass().getResource("FxLogin.fxml");
+		SceneLoader.setScene(stage, path, controller);
+	}
+	
+	@FXML
+	public void addClient() {
+		//TODO update database
+		System.out.println("Add Client");
+	}
+	
+	public void updateInfo() {
+		//User this.username to update all the information
+		System.out.println("Update information for " + this.username);
+		label.setText(this.username);
+		setTab("FxStrength.fxml", strengthTab);
+		setTab("FxEndurance.fxml", enduranceTab);
+		setTab("FxHealth.fxml", healthTab);
+		setTab("FxProgram.fxml", programTab);
+	}
+	
+	
+     
+	
+	
+
 	
 }
