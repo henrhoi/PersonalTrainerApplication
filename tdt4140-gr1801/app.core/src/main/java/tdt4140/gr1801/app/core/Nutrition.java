9f9d67ff82d66a6a3ecba9a5c4eedae7f3db1aff
Sebastian Thorsen Øverås <sebastio@stud.ntnu.no>
2018-03-07 18:40:15 +0100

    Revert "Merge branch 'master' into 'Training'."
    This reverts commit 21e74a298067df949061380457a9aff7798952ca

--- tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Nutrition.java
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/Nutrition.java
@@ -1,34 +1,26 @@
 package tdt4140.gr1801.app.core;
 
+import java.util.Date;
 
 public class Nutrition {
 	
-	private String date; 
+	private Date date; 
 	//everything measured in grams: 
 	private Integer calories; 
 	private Integer fat; 
 	private Integer carbs;
 	private Integer protein; 
-	private Integer clientId;
 	
 	
-	public Nutrition(String date, int calories, int fat, int carbs, int protein, int clientId) {
-		// There are 4 calories per 1 gram carbs,
-		// There are 4 calories per 1 gram protein,
-		// There are 9 calories per 1 gram fat
-		this.clientId = clientId;
-		//if ((9*fat + 4*carbs + 4*protein)==calories) {
+	public Nutrition(Date date, int calories, int fat, int carbs, int protein) {
 		this.date = date;
 		this.calories = calories;
 		this.fat = fat; 
 		this.carbs = carbs;
 		this.protein = protein; 
-		/*} else {
-			throw new IllegalArgumentException("Calories don´t match grams of fat, carbs and proteins.");
-		}*/
 	}
 	
-	public String getDate() {
+	public Date getDate() {
 		return date; 
 	}
 	
@@ -48,9 +40,5 @@ public class Nutrition {
 		return protein;
 	}
 	
-	public int getClientId() {
-		return clientId;
-	}
-
 
 }
