4b69fd21392b5d2c6d9f2e46c23b5c402b499266
kristoffergjerde <kristoffergjerde@dhcp-10-22-33-128.wlan.ntnu.no>
2018-04-14 14:28:16 +0200

    #69 Test for pdfcreator laget
    Sjekker egentlig bare at en fil med et gitt filnavn lagres
    i en fast posisjon. Sletter filen etter på.
    Fjerna også PdfTester.java...

--- tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/pdfcreator/PdfCreator.java
+++ tdt4140-gr1801/app.core/src/main/java/tdt4140/gr1801/app/core/pdfcreator/PdfCreator.java
@@ -76,7 +76,50 @@ public class PdfCreator {
     
     //This method should just write the Healthplan
     public static void addContent(Document document) throws DocumentException {
+    	//Example
+    	Anchor anchor = new Anchor("First Chapter", catFont);
+        anchor.setName("First Chapter");
 
+        // Second parameter is the number of the chapter
+        Chapter catPart = new Chapter(new Paragraph(anchor), 1);
+
+        Paragraph subPara = new Paragraph("Subcategory 1", subFont);
+        Section subCatPart = catPart.addSection(subPara);
+        subCatPart.add(new Paragraph("Hello"));
+
+        subPara = new Paragraph("Subcategory 2", subFont);
+        subCatPart = catPart.addSection(subPara);
+        subCatPart.add(new Paragraph("Paragraph 1"));
+        subCatPart.add(new Paragraph("Paragraph 2"));
+        subCatPart.add(new Paragraph("Paragraph 3"));
+
+        // add a list
+        createList(subCatPart);
+        Paragraph paragraph = new Paragraph();
+        addEmptyLine(paragraph, 5);
+        subCatPart.add(paragraph);
+
+        // add a table
+        createTable(subCatPart);
+
+        // now add all this to the document
+        document.add(catPart);
+
+        // Next section
+        anchor = new Anchor("Second Chapter", catFont);
+        anchor.setName("Second Chapter");
+
+        // Second parameter is the number of the chapter
+        catPart = new Chapter(new Paragraph(anchor), 1);
+
+        subPara = new Paragraph("Subcategory", subFont);
+        subCatPart = catPart.addSection(subPara);
+        subCatPart.add(new Paragraph("This is a very important message"));
+
+        // now add all this to the document
+        document.add(catPart);
+        
+        //Example
     }
 
     public static void createTable(Section subCatPart) throws BadElementException {
