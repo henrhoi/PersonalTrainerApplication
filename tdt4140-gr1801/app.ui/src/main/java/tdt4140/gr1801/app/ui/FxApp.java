4369eb4eb40746c3fc4e1227d2596a5dfdee12b3
Henrik Høiness <henrhoi@stud.ntnu.no>
2018-04-16 11:25:14 +0200

    #70 : Ryddet i kode i FxApp.java, la til kommentarer og en finally-stmt

--- tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/FxApp.java
+++ tdt4140-gr1801/app.ui/src/main/java/tdt4140/gr1801/app/ui/FxApp.java
@@ -8,6 +8,8 @@ import tdt4140.gr1801.web.server.WebServer;
 
 public class FxApp extends Application {
 
+	
+	// Method for opening FxLogin
     @Override
     public void start(Stage stage) throws Exception {
     		LoginController controller = new LoginController();
@@ -17,23 +19,30 @@ public class FxApp extends Application {
     }
 
     public static void main(String[] args) {
-    		Thread thread1 = new Thread () {
-    			public void run () {
-    				WebServer webserver = new WebServer(8080);
-    				try {
-    					try {
-    						webserver.setup();} catch (Exception e) {e.printStackTrace();}
-    						System.out.println("Setting up server");
-        				try {webserver.server.start();} catch (Exception e) {e.printStackTrace();}
-        					System.out.println("Server started");
-        				try {webserver.server.join();} catch (InterruptedException e) {e.printStackTrace();}	
-    				} finally {
-    					webserver.stopServer();
-    					System.out.println("Server stopped");
-    				}
-    		  }
-    		};
-    		thread1.start();
-    		launch(args);
+    		// Creating thread for multithreading server and fxml
+	    	Thread thread = new Thread () {
+				public void run () {
+					// Creating server on port 8080
+					WebServer webserver = new WebServer(8080);
+					try {
+						try {
+							webserver.setup();} catch (Exception e) {e.printStackTrace();}
+						System.out.println("Setting up server");
+						try {webserver.server.start();} catch (Exception e) {e.printStackTrace();}
+						System.out.println("Server started");
+						try {webserver.server.join();} catch (InterruptedException e) {e.printStackTrace();}	
+					} finally {
+						webserver.stopServer();
+						System.out.println("Server stopped");
+					}}};
+					
+		// Trying to start thread and launch LoginFx.fxml
+    		try {
+    			thread.start();
+    			launch(args);
+    			
+    		} finally {
+    			try {thread.join();} catch (InterruptedException e) {e.printStackTrace();}
+    		}
     }
 }
