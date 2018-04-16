b7d26b024566c6f18c626fa20bcfdcb7c46e9255
Henrik Høiness <henrhoi@stud.ntnu.no>
2018-04-16 15:41:14 +0200

    #69 : Prøvd meg på en test i WebServer.java

--- tdt4140-gr1801/web.server/src/test/java/tdt4140/gr1801/web/server/AppTest.java
+++ tdt4140-gr1801/web.server/src/test/java/tdt4140/gr1801/web/server/AppTest.java
@@ -1,38 +1,55 @@
 package tdt4140.gr1801.web.server;
 
-import junit.framework.Test;
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.After;
+import org.junit.Before;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase{
+	Thread thread;
+	
+	@Before
+	protected void setUp() throws InterruptedException, IllegalStateException {
+	    	thread = new Thread () {
+				public void run () {
+					// Creating server on port 8080
+					WebServer webserver = new WebServer(8080);
+					try {
+						try {
+							webserver.setup();} catch (Exception e) {e.printStackTrace();}
+						System.out.println("Setting up server");
+						try {webserver.server.start();} catch (Exception e) {e.printStackTrace();}
+						System.out.println("Server started");
+						try {webserver.server.join();} catch (InterruptedException e) {e.printStackTrace();}	
+					} finally {
+						try {
+							webserver.stopServer();
+							System.out.println("Server stopped");							
+						} catch(IllegalStateException e){
+							System.out.println("Thread stopped");
+							
+						}
+					}}};
+			thread.start();
+			Thread.sleep(2000);
+	}
+	
+	public void testClientResources() throws ClientProtocolException, IOException{
+		String content = GetURL.getRequest("/login/henrhoi");
+		String expected = "{\"Salt\":\"gR+0AXGTRkaGNBktmOoNIaGhddA=\",\"Passwrd\":\"8fd8f77d2bb5ea82471cc18fe80a749311b0819a0938dac935725a84b4c562c3\"}";
+		assertEquals(content, expected);
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	@After
+	public void tearDown() {
+		thread.stop();
+	}
+	
+	
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
